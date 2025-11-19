9917b9e1cad4c83bc55b07601c924f04a61e9512
TinhAnhGitHub <anh.nguyennhu2306@hcmut.edu.vn>
2025-11-19 22:08:26 +0700

    Fix bugs in optimized_main.cpp in strassen openmp

--- openmp-strassen/optimized_main.cpp
+++ openmp-strassen/optimized_main.cpp
@@ -9,8 +9,7 @@
 #define LOWER_B 0.0
 #define UPPER_B 1.0
 
-#define MAX_DEPTH 4
-#define THRESHOLD 256
+#define THRESHOLD 128
 
 class Timer{
     std::chrono::high_resolution_clock::time_point start_;
@@ -24,10 +23,10 @@ class Timer{
     }
 };
 
-std::vector<float> createRandomMatrix(int size, int seed){
-    std::vector<float> matrix(size*size);
+std::vector<double> createRandomMatrix(int size, int seed){
+    std::vector<double> matrix(size*size);
     std::mt19937 rng(seed);
-    std::uniform_real_distribution<float> dist(LOWER_B, UPPER_B);
+    std::uniform_real_distribution<double> dist(LOWER_B, UPPER_B);
     for (int i = 0; i < size * size; ++i) {
         matrix[i] = dist(rng);
     }
@@ -37,17 +36,17 @@ std::vector<float> createRandomMatrix(int size, int seed){
 
 void naiveMultiply(
     int n, 
-    const float* A,
+    const double* A,
     int lda,
-    const float* B,
+    const double* B,
     int ldb,
-    float* C,
+    double* C,
     int ldc
 ){
     #pragma omp parallel for collapse(2) schedule(static) default(none) shared(n, A, B, C, lda, ldb, ldc)
     for(int i = 0; i < n; ++i){
         for(int k = 0; k < n; ++k){
-            float a_ik = A[i * lda + k];
+            double a_ik = A[i * lda + k];
             #pragma omp simd
             for (int j = 0; j < n; ++j){
                 C[i * ldc + j] += a_ik * B[k * ldb + j];
@@ -57,7 +56,7 @@ void naiveMultiply(
 }
 
 void addMatrix(
-    int n, const float* A, int lda, const float* B, int ldb , float* C, int ldc
+    int n, const double* A, int lda, const double* B, int ldb , double* C, int ldc
 ){
     
     for(int i = 0; i < n; i++){
@@ -69,7 +68,7 @@ void addMatrix(
 }
 
 void subtractMatrix(
-    int n, const float* A, int lda, const float* B, int ldb , float* C, int ldc
+    int n, const double* A, int lda, const double* B, int ldb , double* C, int ldc
 ){
     for(int i = 0; i < n; i++){
         #pragma omp simd
@@ -81,45 +80,48 @@ void subtractMatrix(
 
 void strassenSerial(
     int n, 
-    const float* A, int lda,
-    const float* B, int ldb,
-    float* C, int ldc,
-    float* work
+    const double* A, int lda,
+    const double* B, int ldb,
+    double* C, int ldc,
+    double* work
 ){
     if (n <= THRESHOLD){
         naiveMultiply(n, A, lda, B, ldb, C, ldc);
         return;
-        
+    }
+    if (n % 2 != 0) {
+        naiveMultiply(n, A, lda, B, ldb, C, ldc);
+        return;
     }
     int m = n / 2;
-    float* M1 = work; 
-    float* M2 = work + m; 
-    float* M3 = work + 2*m; 
-    float* M4 = work + 3*m;
-    float* M5 = work + 4*m; 
-    float* M6 = work + 5*m;
-    float* M7 = work + 6*m; 
-    float* T1 = work + 7*m; 
-    float* T2 = work + 8*m;
+    double* M1 = work; 
+    double* M2 = work + m*m; 
+    double* M3 = work + 2*m*m; 
+    double* M4 = work + 3*m*m;
+    double* M5 = work + 4*m*m; 
+    double* M6 = work + 5*m*m;
+    double* M7 = work + 6*m*m; 
+    double* T1 = work + 7*m*m; 
+    double* T2 = work + 8*m*m;
 
-    float* nextWork = work + 9*m;
+    double* nextWork = work + 9*m*m;
 
-    const float* A11 = A;             
-    const float* A12 = A + m;
-    const float* A21 = A + m * lda;   
-    const float* A22 = A + m * lda + m;
+    const double* A11 = A;             
+    const double* A12 = A + m;
+    const double* A21 = A + m * lda;   
+    const double* A22 = A + m * lda + m;
     
-    const float* B11 = B;             
-    const float* B12 = B + m;
-    const float* B21 = B + m * ldb;   
-    const float* B22 = B + m * ldb + m;
+    const double* B11 = B;             
+    const double* B12 = B + m;
+    const double* B21 = B + m * ldb;   
+    const double* B22 = B + m * ldb + m;
 
     // M1 = (A11 + A22)(B11 + B22)
     addMatrix(m, A11, lda, A22, lda, T1, m);
     addMatrix(m, B11, ldb, B22, ldb, T2, m);
     strassenSerial(m, T1, m, T2, m, M1, m, nextWork);
 
-    // M2 = (A21 + A22)B11 (Reuse T1)
+    // M2 = (A21 + A22)B11
     addMatrix(m, A21, lda, A22, lda, T1, m);
     strassenSerial(m, T1, m, B11, ldb, M2, m, nextWork);
 
@@ -159,94 +161,103 @@ void strassenSerial(
 
 void strassenParallel(
     int n, 
-    const float* A, int lda,
-    const float* B, int ldb,
-    float* C, int ldc,
-    int depth
+    const double* A, int lda,
+    const double* B, int ldb,
+    double* C, int ldc,
+    int depth,
+    int max_depth
 ){
-    if (depth >= MAX_DEPTH){
-        size_t stackSize  = (size_t)(3 * n * n);
-        std::vector<float> serialStack(stackSize);
-        strassenSerial(n, A, lda, B, ldb, C, ldc, serialStack.data());
+    if (depth >= max_depth || n % 2 != 0) {
+        
+        if (n % 2 == 0) {
+            size_t stackSize = (size_t)(3 * n * n);
+            std::vector<double> serialStack(stackSize);
+            strassenSerial(n, A, lda, B, ldb, C, ldc, serialStack.data());
+        } else {
+            naiveMultiply(n, A, lda, B, ldb, C, ldc);
+        }
         return;
     }
 
     int m = n / 2;
-    std::vector<float> results(17 * m * m);
-    float* M1 = &results[0];         
-    float* M2 = &results[m*m];
-    float* M3 = &results[2*m*m];     
-    float* M4 = &results[3*m*m];
-    float* M5 = &results[4*m*m];     
-    float* M6 = &results[5*m*m];
-    float* M7 = &results[6*m*m];
+    std::vector<double> results(7 * m * m);
+    double* M1 = &results[0];         
+    double* M2 = &results[m*m];
+    double* M3 = &results[2*m*m];     
+    double* M4 = &results[3*m*m];
+    double* M5 = &results[4*m*m];     
+    double* M6 = &results[5*m*m];
+    double* M7 = &results[6*m*m];
 
 
-    const float* A11 = A;             
-    const float* A12 = A + m;
-    const float* A21 = A + m * lda;   
-    const float* A22 = A + m * lda + m;
+    const double* A11 = A;             
+    const double* A12 = A + m;
+    const double* A21 = A + m * lda;   
+    const double* A22 = A + m * lda + m;
     
-    const float* B11 = B;             
-    const float* B12 = B + m;
-    const float* B21 = B + m * ldb;   
-    const float* B22 = B + m * ldb + m;
+    const double* B11 = B;             
+    const double* B12 = B + m;
+    const double* B21 = B + m * ldb;   
+    const double* B22 = B + m * ldb + m;
 
     #pragma omp taskgroup
     {
 
         #pragma omp task shared(results)
         {
-            std::vector<float> T(m * m);
+             // M2 = (A21 + A22)B11
+            std::vector<double> T(m * m);
             addMatrix(m, A21, lda, A22, lda, T.data(), m);
-            strassenParallel(m, T.data(), m, B11, ldb, M2, m, depth + 1);
+            strassenParallel(m, T.data(), m, B11, ldb, M2, m, depth + 1, max_depth);
         }
 
         #pragma omp task shared(results)
         {
-            std::vector<float> T(m * m);
+            // M3 = A11(B12 - B22)
+            std::vector<double> T(m * m);
             subtractMatrix(m, B12, ldb, B22, ldb, T.data(), m);
-            strassenParallel(m, A11, lda, T.data(), m, M3, m, depth + 1);
+            strassenParallel(m, A11, lda, T.data(), m, M3, m, depth + 1, max_depth);
         }
         
         #pragma omp task shared(results)
         {
-            std::vector<float> T(m * m);
+            // M4 = A22(B21 - B11)
+            std::vector<double> T(m * m);
             subtractMatrix(m, B21, ldb, B11, ldb, T.data(), m);
-            strassenParallel(m, A22, lda, T.data(), m, M4, m, depth + 1);
+            strassenParallel(m, A22, lda, T.data(), m, M4, m, depth + 1, max_depth);
         }
 
         #pragma omp task shared(results)
         {
-            std::vector<float> T(m * m);
+            std::vector<double> T(m * m);
             addMatrix(m, A11, lda, A12, lda, T.data(), m);
-            strassenParallel(m, T.data(), m, B22, ldb, M5, m, depth + 1);
+            strassenParallel(m, T.data(), m, B22, ldb, M5, m, depth + 1, max_depth);
         }
 
         #pragma omp task shared(results)
         {
-            std::vector<float> T(2 * m * m);
-            float* t1 = &T[0]; float* t2 = &T[m*m];
+            std::vector<double> T(2 * m * m);
+            double* t1 = &T[0]; double* t2 = &T[m*m];
             subtractMatrix(m, A21, lda, A11, lda, t1, m);
             addMatrix(m, B11, ldb, B12, ldb, t2, m);
-            strassenParallel(m, t1, m, t2, m, M6, m, depth + 1);
+            strassenParallel(m, t1, m, t2, m, M6, m, depth + 1, max_depth);
         }
 
         #pragma omp task shared(results)
         {
-            std::vector<float> T(2 * m * m);
-            float* t1 = &T[0]; float* t2 = &T[m*m];
+            std::vector<double> T(2 * m * m);
+            double* t1 = &T[0]; double* t2 = &T[m*m];
             subtractMatrix(m, A12, lda, A22, lda, t1, m);
             addMatrix(m, B21, ldb, B22, ldb, t2, m);
-            strassenParallel(m, t1, m, t2, m, M7, m, depth + 1);
+            strassenParallel(m, t1, m, t2, m, M7, m, depth + 1, max_depth);
         }
 
         {
-            std::vector<float> T(2 * m * m);
-            float* t1 = &T[0]; float* t2 = &T[m*m];
+            std::vector<double> T(2 * m * m);
+            double* t1 = &T[0]; double* t2 = &T[m*m];
             addMatrix(m, A11, lda, A22, lda, t1, m);
             addMatrix(m, B11, ldb, B22, ldb, t2, m);
-            strassenParallel(m, t1, m, t2, m, M1, m, depth + 1);
+            strassenParallel(m, t1, m, t2, m, M1, m, depth + 1, max_depth);
         }
     }
 
@@ -264,35 +275,62 @@ void strassenParallel(
 
 void strassenMatMul(
     int n, 
-    const std::vector<float>& A, 
-    const std::vector<float>& B, 
-    std::vector<float>& C
+    const std::vector<double>& A, 
+    const std::vector<double>& B, 
+    std::vector<double>& C
 ){
-    int k = 1;
-    int tempN = n;
-    while (tempN > THRESHOLD){
-        k *= 2;
-        tempN /= 2;
+    int paddedSize;
+    int maxDepth = 0;
+    // paddedSize = 1;
+    // while (paddedSize < n) {
+    //     paddedSize *= 2;
+    // }
+    if (n < THRESHOLD)
+    {
+        paddedSize = 1;
+        while (paddedSize < n) {
+            paddedSize *= 2;
+        }
+        maxDepth = 4;
     }
+    else{
+        int k = 1;
+        int tempN = n;
+        
+        while (tempN > THRESHOLD){
+            k *= 2;
+            tempN /= 2;
+        }
+        
 
-    int paddedSize = n;
-    if (n % k != 0){
-        paddedSize = ((n/k)+1) * k;
+        paddedSize = n;
+        if (n % k != 0){
+            paddedSize = ((n/k)+1) * k;
+        }
+        int tmpPaddedsize = paddedSize;
+
+        while (tmpPaddedsize % 2 == 0 && tmpPaddedsize > THRESHOLD){
+            maxDepth ++;
+            tmpPaddedsize /= 2;
+        } 
+
     }
+    
     std::cout << "Original N: " << n << ", Padded N: " << paddedSize << std::endl;
+    std::cout << "Max depth: " << maxDepth << std::endl;
 
     if (paddedSize == n){
         #pragma omp parallel
         {
             #pragma omp single
             {
-                strassenParallel(n, A.data(), n, B.data(), n, C.data(), n, 0);
+                strassenParallel(n, A.data(), n, B.data(), n, C.data(), n, 0, maxDepth);
             }
         }
     } else {
-        std::vector<float> AP(paddedSize * paddedSize, 0.0f);
-        std::vector<float> BP(paddedSize * paddedSize, 0.0f);
-        std::vector<float> CP(paddedSize * paddedSize, 0.0f);
+        std::vector<double> AP(paddedSize * paddedSize, 0.0f);
+        std::vector<double> BP(paddedSize * paddedSize, 0.0f);
+        std::vector<double> CP(paddedSize * paddedSize, 0.0f);
 
         #pragma omp parallel for
         for (int i = 0; i < n; ++i) {
@@ -304,7 +342,7 @@ void strassenMatMul(
         {
             #pragma omp single
             {
-                strassenParallel(n, AP.data(), n, BP.data(), n, CP.data(), n, 0);
+                strassenParallel(paddedSize, AP.data(), paddedSize, BP.data(), paddedSize, CP.data(), paddedSize, 0, maxDepth);
             } 
         }
 
@@ -315,13 +353,19 @@ void strassenMatMul(
     }
 }
 
-int main(){
-    int N = 10000;
+int main(int argc, char ** argv){
+
+    if (argc < 3){
+        std::cerr << "Usage: " << " <matrix_size>" << "  <check err>  ";
+        return 1;
+    }
+    int N = std::atoi(argv[1]);
+
     std::cout << "Initializing..." << std::endl;
-    omp_set_num_threads(20); 
+    omp_set_num_threads(16); 
     auto A = createRandomMatrix(N, 123);
     auto B = createRandomMatrix(N, 456);
-    std::vector<float> C(N * N, 0.0f);
+    std::vector<double> C(N * N, 0.0f);
 
     std::cout << "Starting Strassen..." << std::endl;
     Timer t;
@@ -331,6 +375,22 @@ int main(){
     
     double time = t.elapse();
     std::cout << "Time: " << time << "s" << std::endl;
+
+    int check = std::atoi(argv[2]);
+    if (check==0){
+        return 0;
+    }
+
+    std::vector<double> CC(N * N, 0.0f);
+    naiveMultiply(N, A.data(), N , B.data(), N, CC.data(), N);
+    double diff_sum = 0.0, ref_sum = 0.0;
+    for (int i = 0; i < N * N; ++i) {
+        double diff = C[i] - CC[i];
+        diff_sum += diff * diff;
+        ref_sum += CC[i] * CC[i];
+    }
+    double rel_error = std::sqrt(diff_sum / (ref_sum + 1e-12));
+    std::cout << "Relative L2 error between Strassen and naive: " << rel_error << "\n";
 
     return 0;
 }
